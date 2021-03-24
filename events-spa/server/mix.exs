44ebdf36e5a9b7221399b12e6f6ab484cfb9763c
tanyakler <tkler1919@gmail.com>
2021-03-24 14:57:27 -0400

    added a missing comma

--- events-spa/server/mix.exs
+++ events-spa/server/mix.exs
@@ -44,7 +44,7 @@ defmodule Events.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
       {:cors_plug, "~> 2.0"}
     ]
   end
